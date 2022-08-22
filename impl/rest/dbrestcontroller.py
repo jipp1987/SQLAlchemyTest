112782a2dc56bfb1631f7510c969d5817ced0a50
jipp1987 <jipp1987@gmail.com>
2022-08-22 07:06:15 +0200

    Rest api.

--- impl/rest/dbrestcontroller.py
+++ impl/rest/dbrestcontroller.py
@@ -1,5 +1,6 @@
 import sys
 import traceback
+from functools import wraps
 from typing import List, Union
 
 from flask import Blueprint, make_response, request
@@ -8,12 +9,59 @@ from core.dao.daotools import JsonQuery
 from core.dao.modelutils import serialize_model, BaseEntity
 from core.exception.errorhandler import WrappingException
 from core.rest.apitools import RequestResponse, EnumHttpResponseStatusCodes, DBRequestBody
+from core.service.service import BaseService
 from core.utils.jsonutils import encode_object_to_json, decode_object_from_json
 from impl.rest import servicehandler
 
-
 db_service_blueprint = Blueprint("DBService", __name__, url_prefix='/api/DBService')
 """Blueprint para módulo de api."""
+
+
+def _db_rest_fn(function):
+    """
+    Decorador para tener un cuerpo común para todas las funciones del módulo.
+    :param function: Función a ejecutar.
+    :return: Decorador
+    """
+
+    @wraps(function)
+    def decorator(*args, **kwargs):
+        try:
+            # Obtengo el objeto enviado por json con la petición
+            json_format = encode_object_to_json(request.get_json(force=True))
+            # Luego transformo el string json a un objeto RequestBody, pasando el tipo como parámetro
+            request_body: DBRequestBody = decode_object_from_json(json_format, DBRequestBody)
+
+            # En función de la entidad seleccionada, cargar el servicio correspodiente
+            if request_body.entity is None or not request_body.entity:
+                raise ValueError("You have to specify a target entity.")
+
+            try:
+                service = getattr(servicehandler, f"{request_body.entity}RestService")
+            except AttributeError as e1:
+                raise AttributeError(f"Entity {request_body.entity} does not exist.") from e1
+
+            # Añado los parámetros a la función
+            kwargs['request_body'] = request_body
+            kwargs['service'] = service
+            return function(*args, **kwargs)
+        except (WrappingException, Exception) as e:
+            result: str
+            # Si hay error conocido, pasarlo en el mensaje de error, sino enviar su representación en forma de string.
+            if isinstance(e, WrappingException):
+                print(str(e), file=sys.stderr)
+                error: str = str(e.source_exception) if e.source_exception is not None else str(e)
+                result = error
+            else:
+                print(traceback.print_exc())
+                error: str = str(e)
+                result = error
+
+            response_body: RequestResponse = RequestResponse(response_object=result, success=False,
+                                                             status_code=EnumHttpResponseStatusCodes.BAD_REQUEST.value)
+            return _convert_request_response_to_json_response(response_body)
+
+    return decorator
 
 
 def _convert_request_response_to_json_response(response_body: RequestResponse):
@@ -27,68 +75,48 @@ def _convert_request_response_to_json_response(response_body: RequestResponse):
 
 @db_service_blueprint.route('/delete', methods=['POST'])
 def delete():
-    result: str
-    response_body: Union[RequestResponse, None] = None
 
-    try:
-        # Obtengo el objeto enviado por json con la petición
-        json_format = encode_object_to_json(request.get_json(force=True))
-        # Luego transformo el string json a un objeto RequestBody, pasando el tipo como parámetro
-        request_body: DBRequestBody = decode_object_from_json(json_format, DBRequestBody)
-
-        # En función de la entidad seleccionada, cargar el servicio correspodiente
-        if request_body.entity is None or not request_body.entity:
-            raise ValueError("You have to specify a target entity.")
-
-        try:
-            service = getattr(servicehandler, f"{request_body.entity}RestService")
-        except AttributeError as e1:
-            raise AttributeError(f"Entity {request_body.entity} does not exist.") from e1
+    @_db_rest_fn
+    def __delete(*args, **kwargs): # noqa
+        """
+        Función interna delete.
+        :param request_body:
+        :param service:
+        :return:
+        """
+        request_body: DBRequestBody = kwargs["request_body"]
+        service: BaseService = kwargs["service"]
 
         # Objeto query_object creado a partir del request_object
-        entity_to_delete = service.get_entity_type(**request_body.request_object)
+        entity_to_delete = service.get_entity_type(**request_body.request_object)  # noqa
         service.delete(entity_to_delete)
 
-        json_result = f"{entity_to_delete} has been deleted."
+        json_result = f"'{entity_to_delete}' has been deleted."
         response_body = RequestResponse(response_object=json_result, success=True,
                                         status_code=EnumHttpResponseStatusCodes.OK.value)
 
-    except (WrappingException, Exception) as e:
-        # Si hay error conocido, pasarlo en el mensaje de error, sino enviar su representación en forma de string.
-        if isinstance(e, WrappingException):
-            print(str(e), file=sys.stderr)
-            error: str = str(e.source_exception) if e.source_exception is not None else str(e)
-            result = error
-        else:
-            print(traceback.print_exc())
-            error: str = str(e)
-            result = error
-
-        response_body = RequestResponse(response_object=result, success=False,
-                                        status_code=EnumHttpResponseStatusCodes.BAD_REQUEST.value)
-    finally:
         return _convert_request_response_to_json_response(response_body)
+
+    return __delete()
 
 
 @db_service_blueprint.route('/select', methods=['POST'])
 def select():
-    result: Union[List[dict], List[BaseEntity], str]
-    response_body: Union[RequestResponse, None] = None
 
-    try:
-        # Obtengo el objeto enviado por json con la petición
-        json_format = encode_object_to_json(request.get_json(force=True))
-        # Luego transformo el string json a un objeto RequestBody, pasando el tipo como parámetro
-        request_body: DBRequestBody = decode_object_from_json(json_format, DBRequestBody)
+    @_db_rest_fn
+    def __select(*args, **kwargs): # noqa
+        """
+        Función interior select.
+        :param request_body:
+        :param service:
+        :return: Response.
+        """
+        # En realidad estas dos variables podrían ser los parámetros de la función... pero si no utilizo
+        # "*args, **kwargs" el debugger no sabe llegar hasta aquí sin hacer step into desde el decorador.
+        request_body: DBRequestBody = kwargs["request_body"]
+        service: BaseService = kwargs["service"]
 
-        # En función de la entidad seleccionada, cargar el servicio correspodiente
-        if request_body.entity is None or not request_body.entity:
-            raise ValueError("You have to specify a target entity.")
-
-        try:
-            service = getattr(servicehandler, f"{request_body.entity}RestService")
-        except AttributeError as e1:
-            raise AttributeError(f"Entity {request_body.entity} does not exist.") from e1
+        result: Union[List[dict], List[BaseEntity], str]
 
         # Consulta
         json_result: List[dict] = []
@@ -115,20 +143,9 @@ def select():
                 for r in result:
                     json_result.append(serialize_model(r))
 
-        response_body = RequestResponse(response_object=json_result, success=True,
-                                        status_code=EnumHttpResponseStatusCodes.OK.value)
-    except (WrappingException, Exception) as e:
-        # Si hay error conocido, pasarlo en el mensaje de error, sino enviar su representación en forma de string.
-        if isinstance(e, WrappingException):
-            print(str(e), file=sys.stderr)
-            error: str = str(e.source_exception) if e.source_exception is not None else str(e)
-            result = error
-        else:
-            print(traceback.print_exc())
-            error: str = str(e)
-            result = error
+        response_body: RequestResponse = RequestResponse(response_object=json_result, success=True,
+                                                         status_code=EnumHttpResponseStatusCodes.OK.value)
 
-        response_body = RequestResponse(response_object=result, success=False,
-                                        status_code=EnumHttpResponseStatusCodes.BAD_REQUEST.value)
-    finally:
         return _convert_request_response_to_json_response(response_body)
+
+    return __select()
